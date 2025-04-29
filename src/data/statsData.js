333c467009fdfe76f6da7e81411143992327c7a0
PumpaCto <pumpa6827@gmail.com>
2025-04-29 20:04:28 +0300

    git add src/data/statsData.js git commit -m "data: add project stat metrics for AnimatedStats component"

---
+++ src/data/statsData.js
@@ -0,0 +1,22 @@
+export const statsData = [
+  {
+    label: "Token Holders",
+    value: 8421,
+    suffix: "+",
+  },
+  {
+    label: "Community Members",
+    value: 15700,
+    suffix: "+",
+  },
+  {
+    label: "NFTs Minted",
+    value: 520,
+    suffix: "",
+  },
+  {
+    label: "Anime Views",
+    value: 210000,
+    suffix: "+",
+  }
+];
