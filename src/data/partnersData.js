2c04bf614faefc0c668b9d488b0788825c8187aa
PumpaCto <pumpa6827@gmail.com>
2025-04-29 19:34:32 +0300

    git add src/data/partnersData.js git commit -m "data: add partner logo metadata for Partner section"

---
+++ src/data/partnersData.js
@@ -0,0 +1,34 @@
+export const partnersData = [
+  {
+    name: "RedStar Animation",
+    logo: "/images/partners/redstar.png",
+  },
+  {
+    name: "Pump.fun",
+    logo: "/images/partners/pumpfun.png",
+  },
+  {
+    name: "CryptoGalaxy",
+    logo: "/images/partners/cryptogalaxy.png",
+  },
+  {
+    name: "AnimeChain",
+    logo: "/images/partners/animechain.png",
+  },
+  {
+    name: "SolanaX",
+    logo: "/images/partners/solanax.png",
+  },
+  {
+    name: "OtakuDAO",
+    logo: "/images/partners/otakudao.png",
+  },
+  {
+    name: "MetaVerse Club",
+    logo: "/images/partners/metaverseclub.png",
+  },
+  {
+    name: "NeoAnime Studios",
+    logo: "/images/partners/neoanime.png",
+  }
+];
